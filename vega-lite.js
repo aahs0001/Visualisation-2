f55684642970076cb3db690114f372f6dad97818
aahs0001 <114004910+aahs0001@users.noreply.github.com>
2022-10-13 17:58:00 +0400

    Add files via upload

---
+++ vega-lite.js
@@ -0,0 +1,4 @@
+var vg_3 = "LineChart.vl.json";
+vegaEmbed("#LineChart", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
