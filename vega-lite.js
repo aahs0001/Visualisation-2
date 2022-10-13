0ce16b81d1f0a6723f37355a9428baa6ebb6fdf5
aahs0001 <114004910+aahs0001@users.noreply.github.com>
2022-10-13 19:20:19 +0400

    Delete vega-lite.js

--- vega-lite.js
+++
@@ -1,4 +0,0 @@
-var vg_3 = "LineChart.vl.json";
-vegaEmbed("#LineChart", vg_3).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
